baf24843233801d17f00b34fa9a18abdf54db13f
Brendon Pagano <brendonpagano@gmail.com>
2018-12-15 16:26:57 -0200

    Use ReduxDevTools to inspect fired actions

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { createBrowserHistory } from 'history'
 import { routerMiddleware } from 'connected-react-router'
 import createRootReducer from './rootReducer'
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const history = createBrowserHistory()
 
 const store = createStore(
   createRootReducer(history),
-  compose(applyMiddleware(routerMiddleware(history)))
+  composeEnhancers(applyMiddleware(routerMiddleware(history)))
 )
 
 export { store, history }
